0d588189fdd51cb7da51fce537c7940244cd9a84
Treedill <johannes@tornblom.eu>
2017-03-22 14:45:32 +0100

    Update content.sql

--- content.sql
+++ content.sql
@@ -1,9 +1,3 @@
-INSERT INTO Products (cookie) VALUES('Nut ring');
-INSERT INTO Products (cookie) VALUES('Nut cookie');
-INSERT INTO Products (cookie) VALUES('Amneris');
-INSERT INTO Products (cookie) VALUES('Tango');
-INSERT INTO Products (cookie) VALUES('Almond delight');
-INSERT INTO Products (cookie) VALUES('Berliner');
 
 INSERT INTO Customers (customerName, address) VALUES('Finkakor AB', 'Helsingborg');
 INSERT INTO Customers (customerName, address) VALUES('Småbröd AB', 'Malmö');
@@ -13,6 +7,16 @@ INSERT INTO Customers (customerName, address) VALUES('Kalaskakor AB', 'Trellebor
 INSERT INTO Customers (customerName, address) VALUES('Partykakor AB', 'Kristianstad');
 INSERT INTO Customers (customerName, address) VALUES('Gastkakor', 'Hässleholm');
 INSERT INTO Customers (customerName, address) VALUES('Skånekakor', 'Perstorp');
+
+
+INSERT INTO Products (cookie) VALUES('Nut ring');
+INSERT INTO Products (cookie) VALUES('Nut cookie');
+INSERT INTO Products (cookie) VALUES('Amneris');
+INSERT INTO Products (cookie) VALUES('Tango');
+INSERT INTO Products (cookie) VALUES('Almond delight');
+INSERT INTO Products (cookie) VALUES('Berliner');
+
+
 
 INSERT INTO Ingredients(ingredient) VALUES('Flour');
 INSERT INTO Ingredients(ingredient) VALUES('Butter');
@@ -30,8 +34,9 @@ INSERT INTO Ingredients(ingredient) VALUES('Potato starch');
 INSERT INTO Ingredients(ingredient) VALUES('Wheat');
 INSERT INTO Ingredients(ingredient) VALUES('Sodium bicarbonate');
 INSERT INTO Ingredients(ingredient) VALUES('Vanilla');
-INSERT INTO Ingredients(ingredient) VALUES('Almond delight butter');
 INSERT INTO Ingredients(ingredient) VALUES('Chopped almonds')
 INSERT INTO Ingredients(ingredient) VALUES('Cinnamon');
-INSERT INTO Ingredients(ingredient) VALUES('Berliner');
 INSERT INTO Ingredients(ingredient) VALUES('Vanilla sugar');
+
+
+INSERT INTO RecipeItems (
