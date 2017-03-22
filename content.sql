e8dcc28dd4e15f805820d01e504f68590d12618a
Treedill <johannes@tornblom.eu>
2017-03-22 14:35:31 +0100

    Create content.sql

---
+++ content.sql
@@ -0,0 +1,37 @@
+INSERT INTO Products (cookie) VALUES('Nut ring');
+INSERT INTO Products (cookie) VALUES('Nut cookie');
+INSERT INTO Products (cookie) VALUES('Amneris');
+INSERT INTO Products (cookie) VALUES('Tango');
+INSERT INTO Products (cookie) VALUES('Almond delight');
+INSERT INTO Products (cookie) VALUES('Berliner');
+
+INSERT INTO Customers (customerName, address) VALUES('Finkakor AB', 'Helsingborg');
+INSERT INTO Customers (customerName, address) VALUES('Småbröd AB', 'Malmö');
+INSERT INTO Customers (customerName, address) VALUES('Kaffebröd', 'Landskrona');
+INSERT INTO Customers (customerName, address) VALUES('Bjudkakor AB', 'Ystad');
+INSERT INTO Customers (customerName, address) VALUES('Kalaskakor AB', 'Trelleborg');
+INSERT INTO Customers (customerName, address) VALUES('Partykakor AB', 'Kristianstad');
+INSERT INTO Customers (customerName, address) VALUES('Gastkakor', 'Hässleholm');
+INSERT INTO Customers (customerName, address) VALUES('Skånekakor', 'Perstorp');
+
+INSERT INTO Ingredients(ingredient) VALUES('Flour');
+INSERT INTO Ingredients(ingredient) VALUES('Butter');
+INSERT INTO Ingredients(ingredient) VALUES('Icing sugar');
+INSERT INTO Ingredients(ingredient) VALUES('Roasted, chopped nuts');
+INSERT INTO Ingredients(ingredient) VALUES('Fine-ground nuts');
+INSERT INTO Ingredients(ingredient) VALUES('Ground, roasted nuts');
+INSERT INTO Ingredients(ingredient) VALUES('Bread crumbs');
+INSERT INTO Ingredients(ingredient) VALUES('Sugar');
+INSERT INTO Ingredients(ingredient) VALUES('Egg whites');
+INSERT INTO Ingredients(ingredient) VALUES('Chocolate');
+INSERT INTO Ingredients(ingredient) VALUES('Marzipan');
+INSERT INTO Ingredients(ingredient) VALUES('Eggs');
+INSERT INTO Ingredients(ingredient) VALUES('Potato starch');
+INSERT INTO Ingredients(ingredient) VALUES('Wheat');
+INSERT INTO Ingredients(ingredient) VALUES('Sodium bicarbonate');
+INSERT INTO Ingredients(ingredient) VALUES('Vanilla');
+INSERT INTO Ingredients(ingredient) VALUES('Almond delight butter');
+INSERT INTO Ingredients(ingredient) VALUES('Chopped almonds')
+INSERT INTO Ingredients(ingredient) VALUES('Cinnamon');
+INSERT INTO Ingredients(ingredient) VALUES('Berliner');
+INSERT INTO Ingredients(ingredient) VALUES('Vanilla sugar');
